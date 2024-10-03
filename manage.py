57b338f903e5b6b264619c56942df8db923fa232
mummyk <chibuzoi410@gmail.com>
2024-10-03 13:57:37 +0100

    changed settings

--- manage.py
+++ manage.py
@@ -2,17 +2,11 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from core.settings import base
 
 
 def main():
     """Run administrative tasks."""
-    if base.DEBUG:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                              'core.settings.development')
-    else:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                              'core.settings.production')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
