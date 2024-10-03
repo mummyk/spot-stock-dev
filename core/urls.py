954d8362eb254b3369d7b6b49bcaacb5b522ea53
mummyk <chibuzoi410@gmail.com>
2024-10-03 11:35:30 +0100

    Text

--- core/urls.py
+++ core/urls.py
@@ -14,28 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
-from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("accounts/", include("allauth.urls")),
-    path("", include("users.urls")),
-    path("dashboard/", include("dashboard.urls")),
-    path("company/", include("tenant.urls")),
 ]
-
-
-# In development, serve media files with Django
-if settings.DEBUG:
-    from django.conf.urls.static import static
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
-
-# Add the custom error handlers
-handler404 = 'users.views.handler404'
-handler500 = 'users.views.handler500'
-handler403 = 'users.views.handler403'
-handler400 = 'users.views.handler400'
--- utils/controller/has_permission.py
+++
@@ -1,9 +0,0 @@
-from ninja_extra import permissions
-
-
-class UserWithPermission(permissions.BasePermission):
-    def __init__(self, permission: str) -> None:
-        self._permission = permission
-
-    def has_permission(self, request, view, controller=None):
-        return request.user.has_perm(self._permission)
